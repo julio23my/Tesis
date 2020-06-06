8414c39c71d95fc752cfa2912a064783a1e9c1fd
Julio Moreno <julio23m.y@gmail.com>
2020-06-05 20:18:02 -0400

    login y register

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+#from django.contrib.auth import login as auth_login
 # Create your views here.
 def home_view(request):
     context = {}
     return render(request, 'home.html', context)
 
-def login(request):
+def LoginPage(request):
+    if request.method =='POST':
+        usernames = request.POST['username']
+        passwords = request.POST['password']
+        user = authenticate(request, username=usernames, password=passwords)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or Password is Wrong')
     context = {}
     return render(request, 'login.html',context)
 
@@ -16,6 +29,8 @@ def registration(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'La cuenta fue Creada '+ user)
+            return redirect('login')
     context = {'form':form}
-
     return render(request, 'registration.html',context)
--- admnetworking/urls.py
+++ admnetworking/urls.py
@@ -20,6 +20,6 @@ from accounts import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_view, name='home'),
-    path('login/', views.login, name='login'),
+    path('login/', views.LoginPage, name='login'),
     path('registration/', views.registration, name='registration'),
 ]
